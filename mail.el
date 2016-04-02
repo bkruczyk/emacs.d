a05b264d505d2fb064b773e4f0a174224732d230
Bartłomiej Kruczyk <bartlomiej.kruczyk@gmail.com>
2016-04-02 16:58:32 +0200

    add mail signature

--- mail.el
+++ mail.el
@@ -3,6 +3,8 @@
 (setq user-mail-address "bartlomiej.kruczyk@gmail.com"
       user-full-name "Bartłomiej Kruczyk")
 
+(setq message-signature "z poważaniem,\nBartłomiej Kruczyk")
+
 (setq smtpmail-stream-type 'ssl
       smtpmail-smtp-server "smtp.gmail.com"
       smtpmail-smtp-service 465)

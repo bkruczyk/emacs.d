a86e344e66cb2319c6d05afa32ef55798f83020f
Bartłomiej Kruczyk <bartlomiej.kruczyk@gmail.com>
2016-11-07 22:03:55 +0100

    change evil configuration

--- init.el
+++ init.el
@@ -245,6 +245,7 @@
   (evil-mode +1)
   ;; needed to eval after closing paren in normal mode
   (setq evil-move-beyond-eol t)
+  (setq evil-disable-insert-state-bindings t)
   (add-hook 'paradox-mode-hook #'evil-paredit-mode))
 
 (use-package pkgbuild-mode
